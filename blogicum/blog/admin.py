05fc223b059a6c8c547d7aa12dbf3b8ac9b70c1a
veselovvanya06-lang <veselovvanya06@gmail.com>
2026-03-29 18:13:55 +0300

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,22 +1,19 @@
 from django.contrib import admin
-
-from .models import Category, Location, Post
-
+from .models import Category, Location, Post, Comment
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ('title', 'is_published')
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ('name', 'is_published')
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'pub_date', 'author', 'category', 'is_published')
+    list_editable = ('is_published', 'category')
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('text', 'post', 'author', 'created_at')
